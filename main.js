575dceb4347b86bbf9e0659cbda023baf4508052
KierstonHill83 <khill1218@gmail.com>
2016-04-17 11:08:07 -0600

    Updated projects

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).on('ready', function() {
 
   $('img').hide();
 
+  // Dot & Boxes Game
   $('#mainDot').mouseenter(function() {
     $('#dotGame').show();
   });
@@ -11,6 +12,59 @@ $(document).on('ready', function() {
     $('#dotGame').hide();
   });
 
+  // Bananas & Butterflies
+  $('#mainTranslator').mouseenter(function() {
+    $('#translator').show();
+  });
+
+  $('#mainTranslator').mouseleave(function() {
+    $('#translator').hide();
+  });
+
+  // Active Allies
+  $('#mainAllies').mouseenter(function() {
+    $('#allies').show();
+  });
+
+  $('#mainAllies').mouseleave(function() {
+    $('#allies').hide();
+  });
+
+  // Todo
+  $('#mainTodo').mouseenter(function() {
+    $('#todo').show();
+  });
+
+  $('#mainTodo').mouseleave(function() {
+    $('#todo').hide();
+  });
+
+  // Random Meal Generator
+  $('#mainMeal').mouseenter(function() {
+    $('#meal').show();
+  });
+
+  $('#mainMeal').mouseleave(function() {
+    $('#meal').hide();
+  });
+
+  // WAR
+  $('#mainWar').mouseenter(function() {
+    $('#war').show();
+  });
+
+  $('#mainWar').mouseleave(function() {
+    $('#war').hide();
+  });
+
+  // Rock Paper Scissors
+  $('#mainRock').mouseenter(function() {
+    $('#rock').show();
+  });
+
+  $('#mainRock').mouseleave(function() {
+    $('#rock').hide();
+  });
   
 
 });
